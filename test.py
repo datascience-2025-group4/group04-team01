aa6b3214ec8611748d60196213133f4f78104654
bellala127-glitch <rebecca.halter@stud.uni-heidelberg.de>
2025-05-06 17:10:35 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
 import numpy as np
 import pandas as pd
-
 # columns are separated by tabs not commas
 data = pd.read_csv('data/ab_ag.tsv', sep='\t')
 
-# print first few lines 
-print(data.head())
+# # print first few lines 
+print(data.head(n=4))
 
-# Inspect the dataser ab_ag.tsv
-df = pd.read_csv("data/ab_ag.tsv", sep="\t")
-print(df.shape)
-print(df.columns)
-print(df.head())
+# # Inspect the dataser ab_ag.tsv
+# df = pd.read_csv("data/ab_ag.tsv", sep="\t")
+# print(df.shape)
+# print(df.columns)
+# print(df.head())
